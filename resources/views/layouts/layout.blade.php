a90dea4fe12a33f9c518db5d343e0dadb75af331
KarolZygadlo <karol.zygadlo@blumilk.pl>
2024-04-30 22:26:35 +0200

    added htmx

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,6 +15,7 @@
 
 @include('blocks.footer')
 
+<script src="https://unpkg.com/htmx.org@1.9.12" integrity="sha384-ujb1lZYygJmzgSwoxRggbCHcjc0rB2XoQrxeTUQyRjrOnlCoYta87iKBWq3EsdM2" crossorigin="anonymous"></script>
 <script src="{{ asset('js/wow.min.js') }}"></script>
 <script src="{{ asset('js/swiper-bundle.min.js') }}"></script>
 <script>
