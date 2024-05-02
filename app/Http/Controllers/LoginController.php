69ed627fbc4295f3382d47d0b9fb2fe55c51257e
KarolZygadlo <karol.zygadlo@blumilk.pl>
2024-05-02 19:07:38 +0200

    finished basic auth, added tests

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -1,7 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
 
 class LoginController extends Controller
@@ -11,4 +16,18 @@ class LoginController extends Controller
         return View("login");
     }
 
+    public function store(Request $request): RedirectResponse
+    {
+        $credentials = $request->only("email", "password");
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->route("dashboard");
+        }
+
+        return back()->withErrors([
+            "email" => "The provided credentials do not match our records.",
+        ]);
+    }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <img class="mx-auto h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            <img class="mx-auto h-10 w-auto" src="{{ asset('logo.svg') }}"
                  alt="Your Company"/>
             <h2 class="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your
                 account</h2>
@@ -11,13 +11,21 @@
 
         <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
             <div class="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
-                <form class="space-y-6" action="#" method="POST">
+                <form class="space-y-6" method="POST" action="{{ route('login') }}">
+                    @csrf
+
+                    @if($errors->has("email"))
+                    <div class="bg-wb-red-10 border-wb-red-20 text-wb-grey-80 rounded-md border px-4 py-3 text-center text-sm">
+                        {{ $errors->first("email") }}
+                    </div>
+                    @endif
+
                     <div>
                         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email
                             address</label>
                         <div class="mt-2">
-                            <input id="email" name="email" type="email" autocomplete="email" required=""
-                                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                            <input id="email" name="email" type="email" autocomplete="email" required
+                                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
@@ -25,37 +33,29 @@
                         <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
                         <div class="mt-2">
                             <input id="password" name="password" type="password" autocomplete="current-password"
-                                   required=""
-                                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+                                   required
+                                   class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"/>
                         </div>
                     </div>
 
                     <div class="flex items-center justify-between">
                         <div class="flex items-center">
-                            <input id="remember-me" name="remember-me" type="checkbox"
-                                   class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"/>
+                            <input id="remember-me" name="remember" type="checkbox"
+                                   class="h-4 w-4 rounded border-gray-300 text-sky-700 focus:ring-sky-700"/>
                             <label for="remember-me" class="ml-3 block text-sm leading-6 text-gray-900">Remember
                                 me</label>
                         </div>
 
-                        <div class="text-sm leading-6">
-                            <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
-                        </div>
                     </div>
 
                     <div>
                         <button type="submit"
-                                class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                                class="flex w-full justify-center rounded-md bg-sky-700 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-sky-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
                             Sign in
                         </button>
                     </div>
                 </form>
 
-            <p class="mt-10 text-center text-sm text-gray-500">
-                Not a member?
-                {{ ' ' }}
-                <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">Create a free account</a>
-            </p>
+            </div>
         </div>
-    </div>
 @endsection
