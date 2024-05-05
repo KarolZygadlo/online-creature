9c887ba11e62aa7fa899e7e47ab4acebbbaea50a
Kamil <kamil.piech.97@gmail.com>
2024-05-05 10:16:02 +0200

    #6 - added base form for pdf uploading and preview them

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware("guest")->group(function (): void {
     Route::get("/", [LoginController::class, "create"])->name("home");
     Route::post("/login", [LoginController::class, "store"])->name("login");
+    Route::get("/page", fn() => view("page"))->name("page");
+
 });
 
 Route::middleware("auth")->group(function (): void {
